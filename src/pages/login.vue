33375bde8f6e21094edbf09dedd5b0e8cb992c90
tianluoluo <tianluoluo666@gmail.com>
2018-11-12 14:47:04 +0800

    login(change css))

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,39 +1,38 @@
 <template>
   <div class="wrapper page-login">
-    <fieldset class="formbox">
-      <legend class="msg">用户登录信息</legend>
-      <div class="form">
-        <div class="item">
-          <label for="IP">服务器IP：</label>
-          <input type="text" name="IP" id="IP" v-model="user.IP">
-        </div>
-        <div class="item">
-          <label for="port">服务端口：</label>
-          <input type="text" name="port" id="port" v-model="user.port">
-        </div>
-        <div class="item">
-          <label for="pnum">工作站号：</label>
-          <input type="text" name="pnum" id="pnum"  v-model="user.pnum" @click="showitem">
-          <div class="icon" @click="showitem"></div>
-          <ul class="pnumdata">
-            <li class="dataitem" v-for="(item,index) of pnumdata" :key="index" @click="changeitem">{{ item }}</li>
-          </ul>
-        </div>
-        <div class="item">
-          <label for="jnum">工<i></i>号：</label>
-          <input type="text" name="jnum" id="jnum" v-model="user.jnum">
-        </div>
-        <div class="item">
-          <label for="password">密<i></i>码：</label>
-          <input type="password" name="password" id="password" v-model="user.password">
+    <div class="container">
+      <div class="left">
+        <img class="leftimg" src="../../static/img/loginleft.png" alt="穿梭车">
+        <div class="txt">
+          <img class="logo" src="../../static/img/logoicon.png" alt="九州通">
+          <div class="introC">穿梭车库一体机管理系统</div>
+          <div class="introE">SHUTTLE GARAGE INTEGRATED MANAGEMENT SYSTEM</div>
         </div>
       </div>
-    </fieldset>
-    <div class="btnbox">
-      <button  class="btn" @click="login">登陆</button>
-      <button  class="btn exit" @click="exit">退出</button>
+      <div class="right">
+        <div class="formbox">
+          <div class="title">用户登录</div>
+          <div class="form">
+            <div class="item clear">
+              <input type="text" class="IP" placeholder="服务器IP" name="IP" id="IP" v-model="user.IP">
+              <input type="text" class="port" placeholder="服务端口" name="port" id="port" v-model="user.port">
+            </div>
+            <div class="item">
+              <input type="text" class="pnum" placeholder="工作站号" name="pnum" id="pnum"  v-model="user.pnum">
+            </div>
+            <div class="item">
+              <input type="text" class="jnum" placeholder="请输入工号" name="jnum" id="jnum" v-model="user.jnum">
+            </div>
+            <div class="item">
+              <input type="password" class="password" placeholder="请输入密码" name="password" id="password" v-model="user.password">
+            </div>
+            <div class="item">
+              <button  class="btn" @click="login">登陆</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="hint">温馨提示：欢迎您使用九州通医药集团穿梭车智能控制系统</div>
   </div>
 </template>
 
@@ -55,132 +54,142 @@ export default {
     }
   },
   methods:{
-    // 工作站号
-    showitem () {
-      document.getElementsByClassName('pnumdata')[0].style.display = 'block'
-    },
-    changeitem (e) {
-      this.user.pnum = e.target.innerHTML
-      document.getElementsByClassName('pnumdata')[0].style.display = 'none'
-    },
-        // store
+    // store
     ...mapMutations(['saveUser']),
     // 登陆
     login () {
       this.saveUser(this.user)
       console.log(this.$store.state.user)
       alert('登陆成功')
-    },
-    // 退出
-    exit () {}
+    }
   }
 }
 </script>
 <style lang="less">
 .page-login {
   position: relative;
-  background: #999;
+  background: #e8eaf1;
   height: 100%;
   width: 100%;
-  .formbox {
-    position: absolute;
-    width: 5.6rem;
-    margin: auto;
-    padding: .2rem;
-    height: 3.3rem;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    font-size: .25rem;
-    color: #fff;
-    border-radius: .08rem;
-    border-color: #fff;
-    border-style: solid;
-    border-width: .04rem;
-    .msg {
-      padding:0 .05rem;
-      font-size: .27rem;
-    }
-    .item {
+  .container {
+    width: 78%;
+    margin: 0 auto;
+    padding: 11% 0;
+    clear: both;
+    .left {
+      float: left;
+      height: 5.05rem;
       position: relative;
-      margin: .1rem 0;
-      padding-left: .15rem;
-      label {
-        width: 1.25rem;
-        display: inline-block;
-        i {
-          display: inline-block;
-          padding-left: .5rem;
+      .leftimg {
+        width: auto;
+        height: 5.05rem;
+      }
+      .txt {
+        font-family: "Microsoft YaHei","微软雅黑";
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 1.4rem;
+        top: calc(50% - 0.7rem);
+        text-align: center;
+        color: #fff;
+        background-color: rgba(0,0,0,0.6);
+        padding-top: .17rem;
+        .logo {
+          width: auto;
+          height: .58rem;
         }
-      }
-      input {
-        box-sizing: border-box;
-        width: 3.8rem;
-        height: .5rem;
-        display: inline-block;
-        outline: none;
-        border: .04rem solid #fff;
-        background: #fff;
-      }
-      .icon {
-        position: absolute;
-        top: 0.23rem;
-        right: .4rem;
-        width:0;
-        height:0;
-        border-width:.08rem .08rem 0;
-        border-style:solid;
-        border-color:#333 transparent transparent;
-      }
-      .pnumdata {
-        display: none;
-        position: absolute;
-        top: .5rem;
-        right: .35rem;
-        line-height: .5rem;
-        width: 3.78rem;
-        background: #ccc;
-        color: #333;
-        border-left: .02rem solid #fff;
-        border-right: .02rem solid #fff;
-        z-index: 999;
-        box-sizing: border-box;
-        .dataitem {
-          line-height: .5rem;
-          width: 3.8rem;
-          height: .5rem;
-          background: #ccc;
-          border-bottom: .02rem solid #fff;
+        .introC {
+          font-size: .26rem;
+          font-weight: bold;
+        }
+        .introE {
+          font-size: .13rem;
+          padding-top: .08rem;
+          font-weight: 500;
         }
       }
     }
-  }
-  .btnbox {
-    position: absolute;
-    bottom: .7rem;
-    right: .6rem;
-    .btn {
-      font-size: .25rem;
-      line-height: .25rem;
-      padding: .2rem .4rem;
-      border: none;
-      color: #fff;
-      background-color: #409eff;
-      border-color: #409eff;
-      border-radius: 4px;
+    .right {
+      float: left;
+      .formbox {
+        font-family: "FZLTCHK";
+        width: 4rem;
+        height: 3.45rem;
+        padding: 0.8rem;
+        background-color: #fff;
+        .title {
+          font-size: .26rem;
+          color: rgb(53, 64, 82);
+          font-weight: bolder;
+        }
+        .form {
+          margin-top: .25rem;
+          input{
+            font-family: "Microsoft YaHei","微软雅黑";
+            width: 100%;
+            color: #666;
+            box-sizing: border-box;
+            height: .44rem;
+            line-height: .44rem;
+            padding: .1rem 0 .1rem .4rem;
+            border: .01rem solid rgb(223, 227, 233);
+            margin-top: .14rem;
+            outline: none;
+          }
+          input::-webkit-input-placeholder {
+            color: rgb(184, 187, 192);
+            letter-spacing: .01rem;
+          }
+          input:-moz-placeholder {
+            color: rgb(184, 187, 192);
+            letter-spacing: .01rem;
+          }
+          input::-moz-placeholder {
+            color: rgb(184, 187, 192);
+            letter-spacing: .01rem;
+          }
+          input:-ms-input-placeholder {
+            color: rgb(184, 187, 192);
+            letter-spacing: .01rem;
+          }
+          .clear {
+            clear: both;
+            .IP {
+              float: left;
+              width: 62%;
+              padding-left: .15rem;
+            }
+            .port {
+              float: right;
+              width: 35%;
+              padding-left: .15rem;
+            }
+          }
+          .pnum {
+            background: url(../../static/img/arrowicon.png) no-repeat .14rem 53%;
+          }
+          .jnum {
+            background: url(../../static/img/usericon.png) no-repeat .14rem 53%;
+          }
+          .password {
+            background: url(../../static/img/passicon.png) no-repeat .14rem 53%;
+          }
+          .btn {
+            box-sizing: border-box;
+            width: 100%;
+            height: .44rem;
+            padding: .08rem;
+            font-size: .2rem;
+            margin-top: .14rem;
+            letter-spacing: .05rem;
+            background-color: #198efb;
+            border: none;
+            color: #fff;
+          }
+        }
+      }
     }
-    .exit {
-      background-color: #333;
-      border-color: #fff;
-    }
-  }
-  .hint {
-    position: absolute;
-    bottom: .3rem;
-    left: .4rem;
-    color: red;
-    font-size: .3rem;
   }
 }
 </style>
